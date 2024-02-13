480aa2dfe31dd7c9cfeb4adeac2da0b0402758c6
Frederick <62467001+ToichiKuroba@users.noreply.github.com>
2024-02-13 23:19:37 +0100

    Update Message to 2024

--- src/commands/Valentine/valentineHelp.ts
+++ src/commands/Valentine/valentineHelp.ts
@@ -2,31 +2,31 @@ import { CommandInteraction, Client, ApplicationCommandType, SlashCommandChannel
     TextChannel, PermissionsBitField, EmbedBuilder } from 'discord.js';
 import { Command } from "../../Command";
 
-const content = "<a:ECv_blackrose:1072685008646582394>  **VALENTINE'S CARDS** <a:ECv_purplerose:1072685244278374491> \n\n"
+const content = "<a:ECv_blackrose:1207085877109457077>  **VALENTINE'S CARDS** <a:ECv_purplerose:1207085976644755466> \n\n"
     + "This month, you can send little Valentine's cards to members of The Eumelcord. All cards will be anonymous and will not be "
     + "revealed until the 14th of February. \n\n"
-    + "<:ECv_purpleheart:1072685227757031455> **WHAT IT IS**\n"
+    + "<:ECv_purpleheart:1207085960215662602> **WHAT IT IS**\n"
     + "This little event is meant to spread positivity and kindness. The cards aren't necessarily romantic. "
     + "Their purpose is to bring a smile to someone's face because I imagine, even if you don't know who sent it, "
     + "it'll make you happy to know someone appreciates you and your friendship. Who wouldn't be happy about "
     + "receiving some kind words? \n\n"
-    + "<:ECv_chocoheart:1072685069241700422>  **HOW IT WORKS**\n"
+    + "<:ECv_chocoheart:1207085895904399380>  **HOW IT WORKS**\n"
     + "To send a card, you'll use `/valentine-set`. You can either enter a custom message or choose one from our text options.\n"
     + "> The member field is where you will select the member that shall receive the card.\n"
     + "> The card field is a selection of available little cute messages, which you can freely choose.\n"
     + "After you’re done, click the *Finish* button below. This only works if you've selected a member and entered or selected a "
     + "message. The message will then be scheduled and posted by pinging the selected user on the 14th of February. This is anonym, "
     + "of course, so the pinged user will not know who sent them the card.\n\n"
-    + "<:ECv_neonrose:1072685213513154600>  **THE COMMANDS**\n"
+    + "<:ECv_neonrose:1207085941462925393>  **THE COMMANDS**\n"
     + "`/valentine-set` — to send a card; optionally, you can add a custom text (emojis work as well; for a new line just write `\\n` ; "
     + "you can use all normal inline adjustments like `\*` for bold or italic; mentions of member or roles is also possible).\n"
     + "`/valentine-show` — shows your current cards, IDs, and who you send them to.\n"
     + "`/valentine-help` — posts this explanation as secret message\n"
     + "`/valentine-delete [unique-id]` — deletes one of your cards; just put in the right ID.\n\n"
-    + "<:ECv_chocoheart2:1072685084844490782>  **BADGE**\n"
+    + "<:ECv_chocoheart2:1207085913243648121>  **BADGE**\n"
     + "If you send 14 cards or more, you'll receive a special badge after the event! You can check your card count and look at "
     + "who you have already sent one by using `/valentine-show`. \n\n"
-    + "<:ECv_loveletter:1072685269414846554>  *Cards can be sent until Tuesday, February 14, 2023, 11 PM CET.*";
+    + "<:ECv_loveletter:1207085927634042933>  *Cards can be sent until Wednesday, February 14, 2024, 11 PM CET.*";
 
 export const ValentineHelp: Command = {
     name: "valentine-help",
